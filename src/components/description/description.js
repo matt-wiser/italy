6d5e9643d38b995d619bb51d7f1114003cda2060
Matthew Wiser <matt.wiser@icloud.com>
2022-08-25 03:49:55 -0500

    added initial test of framer motion to section component

--- src/components/description/description.js
+++ src/components/description/description.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { motion } from "framer-motion";
+
 import "./description.css"
 
 const Description = ({ img, children }) => {
   return (
     <section className="description">
-      <div className="description-img">
+      <motion.div className="description-img"
+        initial={{ x: "-100%", opacity:0 }}
+        whileInView={{ x: 0, opacity:1 }}
+        transition={{ ease: "easeOut", duration: .75 }}>
         <img src={img} alt="italy"/>
-      </div>
+      </motion.div>
 
       <div className="description-text">{children}</div>
     </section>
